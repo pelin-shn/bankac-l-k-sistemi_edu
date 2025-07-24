7479c0e5aab11f78b6567a45feff23421b3e710d
pelin <pelinsahiinn@icloud.com>
2025-07-24 15:36:56 +0300

    firt commit

---
+++ banka.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define Kullanici_dosya "kullanici.txt"
+#define log_dosya "log.txt"
+#define Hesap_dosya "account.txt"
+
+typedef struct
+{
+    int ID;
+char Name[50];
+char Surname[50];
+char Gender[50];
+int BirthDay;
+double CurrentBalance;
+   
+} Kullanici;
+
+// kullanıcı girişi fonksiyonu
+
+
+int kullanici_giris() {
+    char username[50], password[50];
+    printf("Kullanici adinizi giriniz: ");
+    scanf("%s", username);
+    printf("Sifrenizi giriniz: ");
+
+    }
+
+if (dosya == NULL) {
+        printf("Dosya bulunamadi.\n");
+        return 0;
+    }
+    
+void kullanici_ekle;(struct Account *account) {
+    FILE *file = fopen(banka, "a");
+    if (file == NULL) {
+        printf("Error opening file!\n");
+        return;
+    } 
+
+
+
+int main() {
+
+}
